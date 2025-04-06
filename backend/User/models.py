70f0af6767b4c21c6fef111e08251d4aee1ff9da
Лев Примак <123818798+Furlos@users.noreply.github.com>
2025-04-06 23:28:02 +0300

    Update models.py

--- backend/User/models.py
+++ backend/User/models.py
@@ -1,1 +1,10 @@
-### START
+from pydantic import BaseModel, Field
+from typing import List
+
+class User(BaseModel):
+    id: int
+    phone: str
+    password: str
+    name: str
+    surname: str
+    registered_event_ids: List[int] = Field(default_factory=list)
