af95b325c2c1708f61ea287eb242876e5df32f13
Лев Примак <123818798+Furlos@users.noreply.github.com>
2025-04-07 00:10:20 +0300

    Update models.py

--- backend/Events/models.py
+++ backend/Events/models.py
@@ -1,1 +1,12 @@
-### Start
+from pydantic import BaseModel, Field
+from typing import List
+from ..mixins import MixinId, MixinStartEndTime
+
+class _EventBaseModel(BaseModel, MixinStartEndTime):
+    name: str
+    description: str
+    location: str
+    price: int
+
+class EventModel(_EventBaseModel):
+    id: MixinId
